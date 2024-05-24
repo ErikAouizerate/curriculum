d1f82f84aeacaf2c80274a927f1a7cc7a605656f
faucher julien <julien.faucher@mintset.io>
2024-05-24 17:17:24 +0200

    fix import

--- src/data/tools.js
+++ src/data/tools.js
@@ -4,13 +4,13 @@ import gitlab from "../../public/icons/gitlab.webp";
 import english from "../../public/icons/english.png";
 import kanban from "../../public/icons/kanban.png";
 import vscode from "../../public/icons/vscode.png";
-import linux from "../../public/icons/linux.png";
-import rgpd from "../../public/icons/rgpd.png";
-import macos from "../../public/icons/macos.jpg";
+// import linux from "../../public/icons/linux.png";
+// import rgpd from "../../public/icons/rgpd.png";
+// import macos from "../../public/icons/macos.jpg";
 import windows from "../../public/icons/windows.webp";
 import figma from "../../public/icons/figma.png";
 import androidStudio from "../../public/icons/androidStudio.png";
-import SVN from "../../public/icons/SVN.png";
+import SVN from "../../public/icons/svn.png";
 import xd from "../../public/icons/xd.png";
 
 import labels from "./labels";
